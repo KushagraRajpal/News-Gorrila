74127c7cb188c7d98cb549598f4d10c2359368a7
KushagraRajpal <kushagrarajpal23@gmail.com>
2021-10-11 12:53:58 +0530

    Refactoring news component

--- src/components/News.js
+++ src/components/News.js
@@ -22,8 +22,8 @@ export class News extends Component {
             page: 1
         }
     }
-    async componentDidMount() {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9f3f7a9cd5c346bdaf9eb471bf562403&page=1&pageSize=${this.props.pageSize}`
+    async updateNews() {
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9f3f7a9cd5c346bdaf9eb471bf562403&page=${this.state.page}&pageSize=${this.props.pageSize}`
         this.setState({ loading: true })
         let data = await fetch(url);
         let parsedData = await data.json()
@@ -33,29 +33,16 @@ export class News extends Component {
             loading: false
         })
     }
+    async componentDidMount() {
+        this.updateNews()
+    }
     handlePrev = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9f3f7a9cd5c346bdaf9eb471bf562403&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`
-        this.setState({ loading: true })
-        let data = await fetch(url);
-        let parsedData = await data.json()
-        this.setState({
-            page: this.state.page - 1,
-            articles: parsedData.articles,
-            loading: false
-        })
+        this.setState({ page: this.state.page - 1})
+        this.updateNews()
     }
     handleNext = async () => {
-        if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9f3f7a9cd5c346bdaf9eb471bf562403&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-            this.setState({ loading: true });
-            let data = await fetch(url);
-            let parsedData = await data.json()
-            this.setState({
-                page: this.state.page + 1,
-                articles: parsedData.articles,
-                loading: false
-            })
-        }
+        this.setState({ page: this.state.page + 1})
+        this.updateNews()
     }
     render() {
         return (
@@ -65,7 +52,7 @@ export class News extends Component {
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => {
                         return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imgUrl={element.urlToImage} newsUrl={element.url} author= {element.author} date = {element.publishedAt}  source = {element.source.name}/>
+                            <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imgUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
                         </div>
                     })}
                 </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -12,12 +12,12 @@ export class NewsItem extends Component {
                         : imgUrl} className="card-img-top" alt="..." />
                     <div className="card-body ">
                         <h5 className="card-title ">{title}
-                         <span className="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-danger">
-                            {source}
-                        </span>
+                            <span className="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-danger">
+                                {source}
+                            </span>
                         </h5>
                         <p className="card-text ">{description}</p>
-                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
+                        <p className="card-text"><small className="text-danger">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
                         <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
